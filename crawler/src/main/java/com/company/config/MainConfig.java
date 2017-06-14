00d3033ec38f7c3779c8347252371805269596bc
dgrachev28 <dgrachev28@gmail.com>
2017-06-15 01:11:35 +0300

    Disable bootRepackage, fix crawler config, add gradle local properties plugin

--- crawler/src/main/java/com/company/config/MainConfig.java
+++ crawler/src/main/java/com/company/config/MainConfig.java
@@ -1,12 +1,9 @@
 package com.company.config;
 
-import com.company.core.repository.PictureRepository;
-import com.company.core.service.FileService;
-import com.company.crawler.ArtCatalogParser;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {PictureRepository.class, ArtCatalogParser.class, FileService.class})
+@ComponentScan(basePackages = {"com.company.crawler", "com.company.component"})
 public class MainConfig {
 }
